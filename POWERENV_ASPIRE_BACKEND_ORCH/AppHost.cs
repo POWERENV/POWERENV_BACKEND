f740363fa522c0cf15c34b899bb285f148d202e3
Eduardo Sá <eduardoxaviersa@gmail.com>
2026-08-18 20:02:00 +0100

    Configuring Docker container compilation in Aspire's Apphost.cs. - Optimizing docker builds using .dockerignore files in each targeted project.

--- POWERENV_ASPIRE_BACKEND_ORCH/AppHost.cs
+++ POWERENV_ASPIRE_BACKEND_ORCH/AppHost.cs
@@ -2,42 +2,25 @@ using ASPIRE_DEVENV_SETUP_MANAGEMENT;
 
 #region REQUIREMENTS_CHECKUP
 
-string? executionMode = Environment.GetEnvironmentVariable("POWERENV_EXECUTION_MODE");
+bool isPublishing = args.Contains("--operation") && args.Contains("publish");
 
-switch (executionMode)
+if (!isPublishing)
 {
-    case null:
-        throw new Exception("POWERENV_EXECUTION_MODE environment variable is not set. Please set the environment variable and restart the application.");
+    string? executionMode = Environment.GetEnvironmentVariable("POWERENV_EXECUTION_MODE");
 
-    case "DEVELOPMENT":
-        string? targetMachineIPAddress = null;
-        string? targetUserName = null;
+    switch (executionMode)
+    {
+        case null:
+            throw new Exception("POWERENV_EXECUTION_MODE environment variable is not set. Please set the environment variable and restart the application.");
 
-        DOCKER_CONTEXT_VERIFIER dockerContextVerifier = new DOCKER_CONTEXT_VERIFIER();
-        SSH_TUNNEL_VERIFIER sshTunnelVerifier = new SSH_TUNNEL_VERIFIER();
+        case "DEVELOPMENT":
+            string? targetMachineIPAddress = null;
+            string? targetUserName = null;
 
-        if (!dockerContextVerifier.CheckCompliance(new DockerContextComplianceCheckArgs("vmware-docker")))
-        {
-            Console.Write("Please provide the target machine IP address: ");
+            DOCKER_CONTEXT_VERIFIER dockerContextVerifier = new DOCKER_CONTEXT_VERIFIER();
+            SSH_TUNNEL_VERIFIER sshTunnelVerifier = new SSH_TUNNEL_VERIFIER();
 
-            while (targetMachineIPAddress == null)
-            {
-                targetMachineIPAddress = Console.ReadLine();
-            }
-
-            Console.Write("Please provide the target machine username: ");
-
-            while (targetUserName == null)
-            {
-                targetUserName = Console.ReadLine();
-            }
-
-            dockerContextVerifier.SetupRequirement(new DockerContextCreateArgs("vmware-docker", targetMachineIPAddress, targetUserName));
-        }
-
-        if (!sshTunnelVerifier.CheckCompliance(new SSHTunnelComplianceCheckArgs()))
-        {
-            if (targetMachineIPAddress == null && targetUserName == null)
+            if (!dockerContextVerifier.CheckCompliance(new DockerContextComplianceCheckArgs("vmware-docker")))
             {
                 Console.Write("Please provide the target machine IP address: ");
 
@@ -52,18 +35,40 @@ switch (executionMode)
                 {
                     targetUserName = Console.ReadLine();
                 }
+
+                dockerContextVerifier.SetupRequirement(new DockerContextCreateArgs("vmware-docker", targetMachineIPAddress, targetUserName));
             }
 
-            sshTunnelVerifier.SetupRequirement(new SSHTunnelCreateArgs(targetMachineIPAddress, targetUserName));
-        }
+            if (!sshTunnelVerifier.CheckCompliance(new SSHTunnelComplianceCheckArgs()))
+            {
+                if (targetMachineIPAddress == null && targetUserName == null)
+                {
+                    Console.Write("Please provide the target machine IP address: ");
 
-        break;
+                    while (targetMachineIPAddress == null)
+                    {
+                        targetMachineIPAddress = Console.ReadLine();
+                    }
 
-    case "PRODUCTION":
-        break;
+                    Console.Write("Please provide the target machine username: ");
 
-    default:
-        throw new Exception($"Invalid POWERENV_EXECUTION_MODE value: {executionMode}. Please set it to either 'DEVELOPMENT' or 'PRODUCTION'.");
+                    while (targetUserName == null)
+                    {
+                        targetUserName = Console.ReadLine();
+                    }
+                }
+
+                sshTunnelVerifier.SetupRequirement(new SSHTunnelCreateArgs(targetMachineIPAddress, targetUserName));
+            }
+
+            break;
+
+        case "PRODUCTION":
+            break;
+
+        default:
+            throw new Exception($"Invalid POWERENV_EXECUTION_MODE value: {executionMode}. Please set it to either 'DEVELOPMENT' or 'PRODUCTION'.");
+    }
 }
 
 #endregion
@@ -72,14 +77,23 @@ switch (executionMode)
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-// 1. Tell Aspire you want to host/publish into a Docker Compose layout
-builder.AddDockerComposeEnvironment("production-env");
+#pragma warning disable ASPIRECOMPUTE003
+
+var registry = builder.AddContainerRegistry(
+    "ghcr",
+    "ghcr.io",
+    "powerenv/powerenv_backend"
+);
+
+builder.AddDockerComposeEnvironment("powerenv-docker-compose");
 
 IResourceBuilder<RedisResource> redisCache = builder.AddRedis("RedisCache").WithContainerRuntimeArgs("-p", "6379:6379"); // Maps all interfaces (0.0.0.0) by default
 
-builder.AddProject<Projects.POWERENV_BACKEND_API>("MAIN-API").WithReference(redisCache);
+builder.AddProject<Projects.POWERENV_BACKEND_API>("MAIN-API").WithReference(redisCache).WithContainerRegistry(registry);
 
-builder.AddProject<Projects.POWERENV_OSCONSOLE_WORKER>("OSCONSOLE-WORKER").WithReference(redisCache);
+builder.AddProject<Projects.POWERENV_OSCONSOLE_WORKER>("OSCONSOLE-WORKER").WithReference(redisCache).WithContainerRegistry(registry);
+
+#pragma warning restore ASPIRECOMPUTE003
 
 builder.Build().Run();
 
